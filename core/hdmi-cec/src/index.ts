bc65a16ad4bfc4b02582b6c9e1c399483d3e6f0f
Manuel Reiländer <manuel.reilaender@compax.at>
2026-03-05 19:07:43 +0100

    remove comments and unused code

--- core/hdmi-cec/src/index.ts
+++ core/hdmi-cec/src/index.ts
@@ -1,25 +1,18 @@
 import BalenaAudio from 'balena-audio'
 import SoundConfig from './SoundConfig'
-import {CecMonitor, OperationCode, Remote} from 'hdmi-cec'
+import {CecMonitor, Remote} from 'hdmi-cec'
 
 // balenaSound core
 const config: SoundConfig = new SoundConfig()
 const audioBlock: BalenaAudio = new BalenaAudio(`tcp:${config.device.ip}:4317`)
 config.bindAudioBlock(audioBlock)
 
-var remote = new Remote()
+const remote = new Remote()
 
 remote.monitor.on('ready', (cecMonitor: CecMonitor) => {
   console.log('CECMonitor is ready!')
 
   config.bindCecMonitor(cecMonitor)
-
-  // cecMonitor.stdinHandlers.push({
-  //   match: /^.*$/g,
-  //   callback: line => { console.log(line) }
-  // })
-
-
 })
 
 init()
@@ -34,22 +27,11 @@ async function init() {
 audioBlock.on('play', async (sink: any) => {
   console.log(`[event] Sound started playing, sending CEC active source`)
 
-  const cecMonitor = config.getCecMonitor()
-  console.log('Cec monitor address: ' + cecMonitor.deviceAddress)
-  cecMonitor.send("as")
-  // cecMonitor.executeOperation(cecMonitor.deviceAddress, OperationCode.ACTIVE_SOURCE)
-  // config.getCecMonitor().executeBroadcastOperation(OperationCode.ACTIVE_SOURCE)
-
-  // remote.monitor.executeBroadcastOperation(OperationCode.ACTIVE_SOURCE)
-  // await sendCecActiveSource()
+  config.getCecMonitor().send("as")
 })
 
 audioBlock.on('stop', async (sink: any) => {
   console.log('[event] Audio has stopped playing, sending CEC inactive source')
-  console.log('sink name:' + sink.name)
 
   config.getCecMonitor().send("is")
-  // remote.monitor.executeBroadcastOperation(OperationCode.INACTIVE_SOURCE)
-
-  // await sendCecInactiveSource()
 })
